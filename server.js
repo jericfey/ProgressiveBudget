fa548c8f532d3949b272db667b237d044d0bb660
Jeri Fey <jericfey@gmail.com>
2021-02-13 17:48:13 -0700

    modified server.js to add compression

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
+// const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,7 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
